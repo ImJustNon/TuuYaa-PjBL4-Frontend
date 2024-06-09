bd8ec8b6f3d2db563ac3d23113b6376763dae40b
ImJustNon <Im_Just_Non@hotmail.com>
2024-06-10 02:43:06 +0700

    Feat : Add i18n

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,6 +1,6 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import resources from "./config/translations/resources";
+import resources from "./config/translation/resources";
 
 
 i18n.use(initReactI18next).init({
